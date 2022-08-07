e8d3f688b35fc97a088d0f0c5063b14d9feabff2
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2022-08-07 22:59:39 +0100

    Complete math_import tutorial

--- math_import.py
+++ math_import.py
@@ -4,3 +4,20 @@ val = 3
 
 sqrt_val = math.sqrt(val)
 print(round(sqrt_val**2))
+
+
+expVal = math.exp(val)
+print(expVal)
+
+#Factorial of val
+facVal = math.factorial(val)
+print(facVal)
+
+sinVal = math.sin(val) #results are in radients
+print(sinVal)
+
+floorVal = math.floor(sinVal)
+print(floorVal)
+
+ceilVal = math.ceil(sinVal)
+print(ceilVal)
