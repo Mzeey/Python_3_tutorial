f2badf8fe94dd32f9754d3252e865b33f62d3fb8
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2022-08-08 11:47:09 +0100

    Add face Deck Scores

--- BlackJack/deck.py
+++ BlackJack/deck.py
@@ -3,6 +3,7 @@ from random import shuffle
 class Deck:
     deck = []
     faceVals = ["A","J","Q","K"]
+    faceScores = {"A": 11, "J":10, "Q":10, "K":10, "2": 2, "3": 3, "4": 4, "5":5, "6":6, "7":7,"8":8,"9":9, "10":10}
     def createDeck(self):
         for i in range(4):
             for card in range(2, 11):
@@ -13,3 +14,6 @@ class Deck:
     
     def shuffleDeck(self):
         shuffle(self.deck)
+        
+    def getFaceScores(self):
+        return self.faceScores
