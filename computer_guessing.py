5eef3757b71ed6bb19ff2afe747732e90ca0f5ce
Chukwudi Nwobodo <mzeeysoftware@gmail.com>
2022-08-08 10:06:30 +0100

    add time taken feature

--- computer_guessing.py
+++ computer_guessing.py
@@ -1,16 +1,18 @@
 from random import random
+from time import clock
 
 rand_val = random()
-
+start_time = clock()
 upper_bound, lower_bound = 1.0, 0.0
 is_guessed = False
 guess = 0.5
 while is_guessed ==False:
+    guess = (upper_bound + lower_bound)/2
     if guess == rand_val:
         print("Number has been found: ", guess)
+        print("Time taken: ", (clock() - start_time))
         is_guessed = True
     elif guess < rand_val:
         lower_bound = guess
     else:
         upper_bound = guess
-    guess = (upper_bound + lower_bound)/2
